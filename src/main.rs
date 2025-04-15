272fe54c99fb61f42616c2c71dbdd45f0425675f
DUYOCSCHOS <duy1632005@gmail.com>
2025-04-15 17:01:15 +0700

    simple fight

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,78 @@
 struct Entity {
-    pub name: &'static str,
-    pub health: i32,
-    pub strength: i32,
+    profile: Profile,
+    health: i32,
+    strength: i32,
+    magic: i32,
+}
+
+impl Entity {
+    fn new(profile: Profile, health: i32, strength: i32, magic: i32) -> Self {
+        Entity {
+            profile,
+            health,
+            strength,
+            magic,
+        }
+    }
+
+    fn name(&self) -> &str {
+        &self.profile.name
+    }
+
+    fn modify_health(&mut self, value: i32) {
+        self.health += value
+    }
+}
+
+struct Profile {
+    pub name: String,
+    pub age: u8,
+    pub religion: String,
 }
 
 fn attack(source: &Entity, target: &mut Entity) {
     let pre_health = target.health;
-    target.health -= source.strength;
-    println!("{} deal {} damage to {} {}->{}", 
-    source.name, 
-    source.strength, 
-    target.name,
+    let dmg = source.strength + source.magic;
+    target.health -= dmg;
+    println!("[{}] deal {} damage to [{}] {}->{}", 
+    source.name(), 
+    dmg, 
+    target.name(),
     pre_health,
     target.health);
 }
 
 fn main() {
-    let mut player: Entity = Entity { name: "David", health: 100, strength: 40 };
-    let mut enemy: Entity = Entity { name: "Goblin", health: 100, strength: 30 };
+    let mut player: Entity = Entity::new(
+        Profile { 
+            name: String::from("David"), 
+            age: 0, 
+            religion: String::from("")
+        }, 
+        100, 
+        40, 
+        20
+    );
+    let mut enemy: Entity = Entity::new( 
+        Profile { 
+            name: String::from("Goblin"), 
+            age: 0, 
+            religion: String::from("")
+        }, 
+        100, 
+        30, 
+        10
+    );
 
     let mut turn = 1;
 
     loop {
         if player.health <= 0 {
-            println!("You lose");
+            println!("[{}] LOSE", player.name());
             break;
         }
         if enemy.health <= 0 {
-            println!("You win");
+            println!("[{}] WIN", player.name());
             break;
         }
 
